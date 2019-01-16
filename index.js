71018657a93e93290c0acf2ba17b5399d3e3821b
Christian B <christianbart13y@gmail.com>
2019-01-16 09:23:48 -0500

    up to date with section 3 lecture 24

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const express = require('express')
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({sup: 'there'});
-});
+passport.use(
+    new GoogleStrategy(
+    {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+    },
+    (accessToken) => {
+        console.log(accessToken);
+    })
+);
+
+//get route handler - first argument is the path; the second is the code we want to be exicuted
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
 
 const PORT = process.env.PORT || 5000
-
-app.listen(PORT);
+app.listen(PORT); 
